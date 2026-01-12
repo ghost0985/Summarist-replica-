44b218f9893bc439a8125d61175269fa0e579fea
Logan <loganheath90@gmail.com>
2026-01-11 16:31:32 -0800

    Stripe fix

--- src/app/api/cancel-subscription/route.ts
+++ src/app/api/cancel-subscription/route.ts
@@ -2,9 +2,7 @@ import { NextResponse } from "next/server";
 import Stripe from "stripe";
 import * as admin from "firebase-admin";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-11-17.clover",
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 if (!admin.apps.length) {
   admin.initializeApp({
